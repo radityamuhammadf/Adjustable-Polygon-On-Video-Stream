bec8872ed3c5c6765af69d0a579dc612fcf6161e
Muhammad Fida Raditya <muhammad.120140037@student.itera.ac.id>
2023-09-18 09:53:57 +0700

    initial video stream

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# importing the Computer Vision module
 import cv2
 
-print("Sup?")
+def stream():
+    # capturing the video from webcam
+    cap = cv2.VideoCapture(0)
+
+    # running infinite loop
+    while True:
+        # reading frames from the video
+        ret, frame = cap.read()
+
+        frame=cv2.resize(frame,(640,480))
+        # displaying the video
+        cv2.imshow('frame', frame)
+
+        # exiting the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # closing the stream
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    stream()
